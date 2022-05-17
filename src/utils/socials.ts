ff9ec64824697cb6508617d17f542a3cb3995536
silksil <silkreulen@gmail.com>
2022-05-17 14:54:41 +0200

    Add github logo

--- src/utils/socials.ts
+++ src/utils/socials.ts
@@ -1,6 +1,8 @@
 import twitter from "@iconify/icons-eva/twitter-fill";
 import linkedin from "@iconify/icons-eva/linkedin-outline";
+import github from "@iconify/icons-eva/github-outline";
 
-export const TWITTER = { name: "Twitter", icon: twitter, url: "https://twitter.com/kmb_world" };
-export const LINKEDIN = { name: "Linkedin", icon: linkedin, url: "https://twitter.com/kmb_world" };
-export const SOCIALS = [TWITTER, LINKEDIN];
+export const TWITTER = { name: "Twitter", icon: twitter, url: "https://twitter.com/sil_kreulen" };
+export const LINKEDIN = { name: "Linkedin", icon: linkedin, url: "https://www.linkedin.com/in/sil-kreulen/" };
+export const GITHUB = { name: "Github", icon: github, url: "https://github.com/silksil" };
+export const SOCIALS = [TWITTER, LINKEDIN, GITHUB];
