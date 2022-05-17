7ee79c0c38ea74327a9867f4930f08cf41f7ba12
silksil <silkreulen@gmail.com>
2022-05-17 18:43:47 +0200

    Update text

--- src/containers/about/SoftSkills.tsx
+++ src/containers/about/SoftSkills.tsx
@@ -6,8 +6,8 @@ const Box = styled("div")(({ theme }) => ({
   borderRadius: theme.shape.borderRadiusMd,
   padding: theme.spacing(1, 2),
   minHeight: 370,
-
   textAlign: "center",
+
   background: alpha(theme.palette.primary.main, 0.05),
   "&:hover": {
     background: alpha(theme.palette.primary.main, 0.2)
@@ -44,13 +44,13 @@ const strenghts = [
 ];
 
 const weaknesses = [
-  { title: "Specific skills and knowledge", description: "I am lacking a broad base of technical knowledge: I have no computer science degree and have mostly focused on frontend development" },
-  { title: "Perfectionism  ", description: "I've worked a year on a personal project that I never shipped due to thinking too big and perfectionism. Conciously trying to work on this through reflection and building in public." }
+  { title: "Specific skills and knowledge", description: "Because I have no computer science degree and have focused on frontend development, I am lacking a broad base of technical knowledge." },
+  { title: "Shipping fast  ", description: "I've worked a year on a personal project that I never shipped due to thinking too big and perfectionism. Conciously trying to work on this through reflection and building in public." }
 ];
 
 const opportunities = [
-  { title: "Leadership", description: "I invested time and energy in improving my leadership skills last year. Nonetheless, there is still a lot of room for improvement." },
-  { title: "Product/UX ", description: "Because I get excited about good UX, and have a good understanding of user needs, I see an opportunity in developing my product management and UX skills." }
+  { title: "Leadership", description: "Last year I purposively decided to invest time in learning how to lead through reading books and following a course. Made a good start, but I still have a lot of learning to do." },
+  { title: "Product/UX ", description: "Because I get excited about good UX, and have a good understanding of user needs, I see an opportunity in further developing my product management and UX skills." }
 ];
 
 const threats = [{ title: "Energy management", description: "Enthusiasm and drive can cause me to work too much and take too little rest. This can negatively impact my mental health and my productivity. Working on this through developing certain habits (e.g. meditation) and reflection." }];
